0eb7f56e169488c6f82e0572bae61e9020a0d118
wwwjf <wwwjf2015@outlook.com>
2018-06-01 10:46:36 +0800

    下拉刷新demo

--- app/src/main/java/com/wengjianfeng/bingpicture/MainActivity.java
+++ app/src/main/java/com/wengjianfeng/bingpicture/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wengjianfeng.bingpicture;
 
 import android.graphics.Color;
+import android.renderscript.RenderScript;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -22,6 +23,10 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private PictureAdapter mPictureAdapter;
     private List<String> mDataList;
     private String pictureListPath = "/v1/blur?d=%s&r=0";
+    private PtrClassicFrameLayout mPtrFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 //        setSupportActionBar(mToolbar);
+
+        //设置DrawerLayout
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
 
         mDataList = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
@@ -59,13 +71,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
                 Log.d(TAG, "onItemClick: adapter click");
+                Toast.makeText(MainActivity.this,
+                        "onItemClick: adapter click",
+                        Toast.LENGTH_SHORT).show();
             }
         });
-        //设置DrawerLayout
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawerLayout.setDrawerListener(toggle);
-        toggle.syncState();
 
+        mPtrFrame = findViewById(R.id.store_house_ptr_frame);
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(final PtrFrameLayout frame) {
+                //
+                frame.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        frame.refreshComplete();
+                    }
+                }, 1800);
+
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        //自动刷新
+        /*mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                 mPtrFrame.autoRefresh();
+            }
+        }, 100);
+*/
     }
 }
